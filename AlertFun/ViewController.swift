60aa8e51288d7b9ef133ad52db8c7758beca6eca
Chloe Crawford <ccrawford3@zagmail.gonzaga.edu>
2020-09-28 11:39:40 -0700

    Initial Commit

---
+++ AlertFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertFun
+//
+//  Created by Crawford, Chloe on 9/28/20.
+//  Copyright © 2020 Crawford, Chloe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
